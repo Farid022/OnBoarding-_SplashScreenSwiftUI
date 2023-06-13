c860329700ec5922ba61295e58a2d23e7a8741d0
Muhammad Farid Ullah <faridkhan125janan@gmail.com>
2023-06-13 10:42:55 +0500

    Initial Commit

---
+++ OnBoarding&_SplashScreenSwiftUI/OnBoarding__SplashScreenSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  OnBoarding__SplashScreenSwiftUIApp.swift
+//  OnBoarding&_SplashScreenSwiftUI
+//
+//  Created by Muhammad Farid Ullah on 13/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct OnBoarding__SplashScreenSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
